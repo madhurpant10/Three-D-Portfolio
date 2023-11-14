16fbe7599fd1857cdc93637fd9e0948adf7df61c
madhurpant <madhurp321@gmail.com>
2023-11-14 11:01:47 +0530

    Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+
+import Navbar from "./components/Navbar";
+import { Home, About, Projects, Contact } from "./pages";
 
 const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline text-red-400">
-        Hello world!
-    </h1>
-  )
-}
+    <main className='bg-slate-300/20'>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/about' element={<About />} />
+          <Route path='/projects' element={<Projects />} />
+          <Route path='/contact' element={<Contact />} />
+        </Routes>
+      </Router>
+    </main>
+  );
+};
 
-export default App
+export default App;
