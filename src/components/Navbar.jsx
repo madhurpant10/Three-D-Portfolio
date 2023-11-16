b82dc1914b426e5a9a91d31b889de5150a32c974
madhurpant <madhurp321@gmail.com>
2023-11-16 13:14:18 +0530

    Contact Page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className='header'>
-      <NavLink to='/'>
-        <p className="blue-gradient_text">MP</p>
+      <NavLink to='/' className="w-40 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md">
+        <p className="blue-gradient_text">Madhur Pant</p>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
         <NavLink to='/about' className={({ isActive }) => isActive ? "text-blue-600" : "text-black" }>
