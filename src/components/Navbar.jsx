9045c2170412a9adf79f742e7172d1e8190d70d1
madhurpant <madhurp321@gmail.com>
2023-11-22 14:10:31 +0530

    Fixes

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -3,7 +3,7 @@ import { NavLink } from "react-router-dom";
 const Navbar = () => {
   return (
     <header className='header'>
-      <NavLink to='/' className="w-40 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md">
+      <NavLink to='/Three-D-Portfolio' className="w-40 h-10 rounded-lg bg-white flex items-center justify-center font-bold shadow-md">
         <p className="blue-gradient_text">Madhur Pant</p>
       </NavLink>
       <nav className='flex text-lg gap-7 font-medium'>
